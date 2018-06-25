3b2c24cd274553d793415dafed91d0f86e052ca5
luhuamin <iioveyou123>
2018-06-25 16:41:46 +0800

    neo4j数据提交和打包

--- src/main/scala/utils/DBConnectionPool.scala
+++ src/main/scala/utils/DBConnectionPool.scala
@@ -9,7 +9,7 @@ import java.util.{LinkedList, ResourceBundle}
   * 时间：2016-07-09
   */
 object DBConnectionPool {
-  private val reader = ResourceBundle.getBundle("connection")
+  /*private val reader = ResourceBundle.getBundle("connection")
   private val max_connection = reader.getString("neo4j.max_connection") //连接池总数
   private val connection_num = reader.getString("neo4j.connection_num") //产生连接数
   private var current_num = 0 //当前连接池已产生的连接数
@@ -17,16 +17,28 @@ object DBConnectionPool {
   private val driver = reader.getString("neo4j.driver")
   private val url = reader.getString("neo4j.url")
   private val username = reader.getString("neo4j.username")
-  private val password = reader.getString("neo4j.password")
+  private val password = reader.getString("neo4j.password")*/
+  //代码替换
+  private val max_connection = 25 //连接池总数
+  private val connection_num = 23 //产生连接数
+  private var current_num = 0 //当前连接池已产生的连接数
+  private val pools = new LinkedList[Connection]() //连接池
+  private val driver = "org.neo4j.driver.v1.Driver"
+  private val url = "jdbc:neo4j:bolt:10.10.206.35:7687"
+  private val username = "neo4j"
+  private val password = "1qaz2wsx"
+
+
   /**
     * 加载驱动
     */
   private def before() {
     if (current_num > max_connection.toInt && pools.isEmpty()) {
-      print("busyness")
+      println("=================================busyness=================================")
       Thread.sleep(2000)
       before()
     } else {
+      println("=================before===================================================")
       Class.forName(driver)
     }
   }
@@ -34,6 +46,7 @@ object DBConnectionPool {
     * 获得连接
     */
   private def initConn(): Connection = {
+    println("=================initConn===================================================")
     val conn = DriverManager.getConnection(url, username, password)
     conn
   }
@@ -41,10 +54,12 @@ object DBConnectionPool {
     * 初始化连接池
     */
   private def initConnectionPool(): LinkedList[Connection] = {
+    println("=================initConnectionPool===================================================")
     AnyRef.synchronized({
       if (pools.isEmpty()) {
         before()
         for (i <- 1 to connection_num.toInt) {
+          println("============================foreach " + i)
           pools.push(initConn())
           current_num += 1
         }
@@ -56,6 +71,7 @@ object DBConnectionPool {
     * 获得连接
     */
   def getConn():Connection={
+    println("============================getConn============================" )
     initConnectionPool()
     pools.poll()
   }
@@ -63,6 +79,7 @@ object DBConnectionPool {
     * 释放连接
     */
   def releaseCon(con:Connection){
+    println("============================releaseCon============================" )
     pools.push(con)
   }
 
