bacb9acfcb982ac531789b25facfd3e39eb4b1ef
luhuamin <iioveyou123>
2018-06-25 09:58:27 +0800

    使用连接池的方式

---
+++ src/main/scala/utils/DBConnectionPool.scala
@@ -0,0 +1,69 @@
+package utils
+
+import java.sql.{Connection, DriverManager}
+import java.util.{LinkedList, ResourceBundle}
+
+/**
+  * 数据库连接池工具类
+  * 语言：scala
+  * 时间：2016-07-09
+  */
+object DBConnectionPool {
+  private val reader = ResourceBundle.getBundle("connection")
+  private val max_connection = reader.getString("neo4j.max_connection") //连接池总数
+  private val connection_num = reader.getString("neo4j.connection_num") //产生连接数
+  private var current_num = 0 //当前连接池已产生的连接数
+  private val pools = new LinkedList[Connection]() //连接池
+  private val driver = reader.getString("neo4j.driver")
+  private val url = reader.getString("neo4j.url")
+  private val username = reader.getString("neo4j.username")
+  private val password = reader.getString("neo4j.password")
+  /**
+    * 加载驱动
+    */
+  private def before() {
+    if (current_num > max_connection.toInt && pools.isEmpty()) {
+      print("busyness")
+      Thread.sleep(2000)
+      before()
+    } else {
+      Class.forName(driver)
+    }
+  }
+  /**
+    * 获得连接
+    */
+  private def initConn(): Connection = {
+    val conn = DriverManager.getConnection(url, username, password)
+    conn
+  }
+  /**
+    * 初始化连接池
+    */
+  private def initConnectionPool(): LinkedList[Connection] = {
+    AnyRef.synchronized({
+      if (pools.isEmpty()) {
+        before()
+        for (i <- 1 to connection_num.toInt) {
+          pools.push(initConn())
+          current_num += 1
+        }
+      }
+      pools
+    })
+  }
+  /**
+    * 获得连接
+    */
+  def getConn():Connection={
+    initConnectionPool()
+    pools.poll()
+  }
+  /**
+    * 释放连接
+    */
+  def releaseCon(con:Connection){
+    pools.push(con)
+  }
+
+}
